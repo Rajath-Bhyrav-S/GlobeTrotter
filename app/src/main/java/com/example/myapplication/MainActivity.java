da77c3f9c1437529fa47481d6316a7cd71e29719
Rajath-Bhyrav-S <rajathbhyrav147@outlook.com>
2024-02-23 21:35:24 +0530

    My first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button startbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startbtn = findViewById(R.id.start_button);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent categoryIntent = new Intent(MainActivity.this, CategoriesActivity.class);
+                startActivity(categoryIntent);
+            }
+        });
+    }
+}
